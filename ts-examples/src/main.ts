d2fdf020854d26603894f822bb64f27ba8d0801e
Sumit Khandelwal <synergy2411@outlook.com>
2025-05-13 13:27:52 +0530

    app added

--- ts-examples/src/main.ts
+++ ts-examples/src/main.ts
@@ -1,103 +1,30 @@
-// Explicit types
-let username: string = "John Doe";
+window.onload = function () {
+  const titleEl = document.getElementById("txtTitle") as HTMLInputElement;
+  const amountEl: HTMLInputElement = document.getElementById(
+    "txtAmount"
+  ) as HTMLInputElement;
+  const dateEl: HTMLInputElement = document.getElementById(
+    "txtDate"
+  ) as HTMLInputElement;
+  const btnEl: HTMLButtonElement = document.getElementById(
+    "btnSubmit"
+  ) as HTMLButtonElement;
 
-username = "Jenny Doe";
+  const listContainerEl: HTMLUListElement = document.getElementById(
+    "list-container"
+  ) as HTMLUListElement;
 
-// implicit types - type inference
-let marks = 99;
-
-// marks = true;
-
-console.log(marks);
-
-class Student {}
-
-let john: Student = new Student();
-
-let mystryVar: unknown;
-
-mystryVar = "Hello";
-
-if (typeof mystryVar === "string") {
-  console.log(mystryVar.toUpperCase());
-}
-
-mystryVar = 201;
-
-mystryVar = true;
-
-let anyVar: any;
-
-anyVar = "Hello";
-
-anyVar = 101;
-
-anyVar = true;
-
-let stringVar: string = "";
-
-let numberVar: number = 201;
-
-let booleanVar: boolean = false;
-
-let tupleVar: [string, number] = ["test@test", 23];
-
-let unionType: string | number = 32;
-
-unionType = "Thirty-two";
-
-let user: { firstName: string; lastName: string };
-
-user = {
-  firstName: "john",
-  // middleName : "",
-  lastName: "doe",
-};
-
-let friends: Array<string>;
-
-friends = ["Monica", "Joey", "Ross"];
-
-let numbers: number[];
-
-numbers = [99, 98, 97, 100];
-
-let demoFn: () => string;
-
-demoFn = () => {
-  return "";
-};
-
-// Never return any value
-function throwErrorAlways(): never {
-  throw new Error("Throwing Error");
-}
-
-// Custom Types
-type User = {
-  email: string;
-  age: number;
-  isAdmin: boolean;
-};
-
-let me: User;
-
-me = {
-  email: "test@test",
-  age: 21,
-  isAdmin: true,
-};
-
-interface IEmployee {
-  empId: string;
-  salary: number;
-  name: string;
-}
-
-let jenny: IEmployee;
-
-jenny = {
-  empId: "e001",
-  name: "Jenny Doe",
-  salary: 20,
+  btnEl.addEventListener("click", (event) => {
+    event.preventDefault();
+    console.log("Title : ", titleEl.value);
+    console.log("Amount : ", amountEl.value);
+    console.log("Date: ", dateEl.value);
+    const listEl = document.createElement("li");
+    listEl.innerHTML = `
+      <h3>${titleEl.value}</h3>
+      <p>Amount : ${amountEl.value}</p>
+      <p>Date : ${dateEl.value}</p>
+    `;
+    listContainerEl.appendChild(listEl);
+  });
 };
---
+++ ts-examples/src/syntax-examples.ts
@@ -0,0 +1,106 @@
+// Explicit types
+let username: string = "John Doe";
+
+username = "Jenny Doe";
+
+// implicit types - type inference
+let marks = 99;
+
+// marks = true;
+
+console.log(marks);
+
+class Student {}
+
+let john: Student = new Student();
+
+let mystryVar: unknown;
+
+mystryVar = "Hello";
+
+if (typeof mystryVar === "string") {
+  console.log(mystryVar.toUpperCase());
+}
+
+mystryVar = 201;
+
+mystryVar = true;
+
+let anyVar: any;
+
+anyVar = "Hello";
+
+anyVar = 101;
+
+anyVar = true;
+
+let stringVar: string = "";
+
+let numberVar: number = 201;
+
+let booleanVar: boolean = false;
+
+let tupleVar: [string, number] = ["test@test", 23];
+
+let unionType: string | number = 32;
+
+unionType = "Thirty-two";
+
+// Custom Object type
+let user: { firstName: string; lastName: string };
+
+user = {
+  firstName: "john",
+  // middleName : "",
+  lastName: "doe",
+};
+
+// Custom Array Type
+let friends: Array<string>;
+
+friends = ["Monica", "Joey", "Ross"];
+
+let numbers: number[];
+
+numbers = [99, 98, 97, 100];
+
+// Function Type
+let demoFn: () => string;
+
+demoFn = () => {
+  return "";
+};
+
+// Never - never returns any value
+function throwErrorAlways(): never {
+  throw new Error("Throwing Error");
+}
+
+// Custom Types
+type User = {
+  email: string;
+  age: number;
+  isAdmin: boolean;
+};
+
+let me: User;
+
+me = {
+  email: "test@test",
+  age: 21,
+  isAdmin: true,
+};
+
+interface IEmployee {
+  empId: string;
+  salary: number;
+  name: string;
+}
+
+let jenny: IEmployee;
+
+jenny = {
+  empId: "e001",
+  name: "Jenny Doe",
+  salary: 20,
+};
