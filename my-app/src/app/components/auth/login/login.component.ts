ece0aa039f79e31bb53e1355a17664c8a72cbb0c
Sumit Khandelwal <synergy2411@outlook.com>
2025-05-19 11:38:38 +0530

    form state added

--- my-app/src/app/components/auth/login/login.component.ts
+++ my-app/src/app/components/auth/login/login.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
+import { NgForm } from '@angular/forms';
+// For local bootstrap loading for this component only
+// Also remove it from angular.json file
+// import "bootstrap/dist/css/bootstrap.min.css";
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
+  encapsulation: ViewEncapsulation.Emulated,
 })
 export class LoginComponent {
-
+  onSubmitForm(loginForm: NgForm) {
+    console.log('Email : ', loginForm.value.email);
+    console.log('Password : ', loginForm.value.password);
+  }
 }
