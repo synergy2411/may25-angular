a08142e00c3931e0a48423fd08d259a85d17bc30
Sumit Khandelwal <synergy2411@outlook.com>
2025-05-19 12:29:15 +0530

    reactive form added

--- my-app/src/app/components/auth/register/register.component.ts
+++ my-app/src/app/components/auth/register/register.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrl: './register.component.css',
 })
 export class RegisterComponent {
+  username = new FormControl();
+  password = new FormControl();
 
+  registrationForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.registrationForm = this.fb.group({
+      username: this.username,
+      password: this.password,
+    });
+  }
+
+  onSubmit() {
+    console.log(this.registrationForm);
+  }
 }
